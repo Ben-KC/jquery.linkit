9334241f037ddde0e1c81cb378842d415862acff
Benjamin Cox <benny24c@gmail.com>
2016-04-19 14:15:42 -0400

    built version 0.1.0

---
+++ jquery.linkIt.js
@@ -0,0 +1,34 @@
+/*
+* name: linkIt
+* author: Ben Cox
+* version: 0.1.0
+* license: MIT
+*/
+
+(function($) {
+	$.fn.linkIt = function(options) {
+		//default settings
+		var settings = $.extend({
+			href		:null,
+			text		:null,
+			target		:'_self'
+		}, options);
+
+		//validate
+		if(settings.href == null) {
+			console.log('You need an href option for LinkIt to work');
+			return this;
+		}
+
+		return this.each(function() {
+			var object = $(this);
+
+			if (settings.text == null) {
+				settings.text = object.text;
+			}
+
+			object.wrap('<a target="' + settings.target + '" href="' + settings.href + '"></a>').text(settings.text);
+		});
+
+	};
+}(jQuery));
